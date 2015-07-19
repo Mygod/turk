aad88788eef75e8d74a9fc8b9afedf74a90ac7dc
Mygod <mygodstudio@gmail.com>
2015-07-19 16:52:07 +0800

    Better camera angle

--- instrument.js
+++ instrument.js
@@ -91,8 +91,8 @@ function init() {
   camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
   scene = new THREE.Scene();
 
+  camera.position.x = -200;
   camera.position.y = 200;
-  camera.position.z = 200;
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   // start the renderer
@@ -165,7 +165,6 @@ function addCannon() {
 
 function addKeys() {
   for (var i = 0; i < numKeys; i++) {
-    // use linear interpolation to find key length
     var k = Math.pow(2, -i / 48);
 
     var key = new THREE.Mesh(
